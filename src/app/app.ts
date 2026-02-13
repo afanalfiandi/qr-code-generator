280655458c5d531d0d8324fbd384be904f735ff1
Afan Alfi Andi <alfiandiafan@gmail.com>
2026-02-13 20:18:51 +0700

    improve scanner and generate

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,32 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, model, signal, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { QRCodeComponent } from 'angularx-qrcode';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BrowserMultiFormatReader } from '@zxing/library';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, QRCodeComponent, FormsModule],
+  imports: [CommonModule, QRCodeComponent, FormsModule, ZXingScannerModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   @ViewChild('qrWrapper', { static: false }) qrWrapper!: ElementRef;
+  currentDevice: MediaDeviceInfo | undefined;
+  qrReader = new BrowserMultiFormatReader;
 
   protected title = 'qr-code-generator';
 
-  qrText: string = '';
-  generatedText: string = '';
-  showError: boolean = false;
+  qrText = model<string>('');
+  generatedText = signal<string>('');
+  showError = signal<boolean>(false);
+  isScanMode = signal<boolean>(false);
+  activeTab = signal<number>(0);
+  imgResult!: string;
+  isScanSuccess = signal<boolean>(false);
+  pushToScanResult = signal<boolean>(false);
+  isScanModalOpen = signal<boolean>(false);
+
+  tabs: {
+    label: string;
+    value: number
+  }[] = [
+      {
+        label: "Generate QR",
+        value: 0
+      },
+      {
+        label: "Scan QR",
+        value: 1
+      },
+    ];
 
   generateQr() {
-    if (!this.qrText || this.qrText.trim() === '') {
-      this.showError = true;
+    if (!this.qrText() || this.qrText().trim() === '') {
+      this.showError.set(true);
       return;
     }
 
-    this.showError = false;
+    this.showError.set(false);
     this.generatedText = this.qrText;
+    this.activeTab.set(0);
+    this.pushToScanResult.set(false);
   }
 
   downloadQR() {
@@ -48,5 +75,95 @@ export class App {
       now.getMinutes().toString().padStart(2, '0'); link.href = image;
     link.download = 'qr-code' + timestamp + '.png';
     link.click();
+  }
+
+  onScan() {
+    this.isScanMode.set(true);
+  }
+
+  handleQrCodeResult(result: string) {
+    console.log('QR Result:', result);
+    this.isScanMode.set(false);
+
+    this.qrText.set(result);
+    this.isScanSuccess.set(true);
+    this.pushToScanResult.set(true);
+    this.closeScanModal();
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    if (event.dataTransfer?.files.length) {
+      this.readFile(event.dataTransfer.files[0]);
+    }
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.readFile(file);
+    }
+  }
+
+  readFile(file: File) {
+    const reader = new FileReader();
+
+    reader.onload = async () => {
+      const img = new Image();
+      img.src = reader.result as string;
+
+      img.onload = async () => {
+        try {
+          const result = await this.qrReader.decodeFromImageElement(img);
+          this.showToast('success', 'Success scan QR from the file!');
+          this.imgResult = img.src;
+          this.qrText.set(result.getText());
+          this.isScanSuccess.set(true);
+          this.pushToScanResult.set(true);
+          // this.generatedText.set('')
+        } catch (err) {
+          this.showToast('error', 'Failed to scan QR from the file!');
+          console.error("QR not detected", err);
+        }
+      };
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  private showToast(icon: 'success' | 'error' | 'info', message: string) {
+    Swal.fire({
+      toast: true,
+      position: 'top-end',
+      icon: icon,
+      title: message,
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+    });
+  }
+
+  onCodeResult(result: string) {
+    console.log('QR Result:', result);
+
+    this.showToast('success', 'QR Code scanned successfully!');
+
+    this.generatedText.set(result);
+  }
+
+  onCopy() {
+
+  }
+
+  openScanModal() {
+    this.isScanModalOpen.set(true);
+  }
+
+  closeScanModal() {
+    this.isScanModalOpen.set(false);
   }
 }
